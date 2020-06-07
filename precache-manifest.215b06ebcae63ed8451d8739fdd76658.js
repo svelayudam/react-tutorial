e7df9f89cd30743768c52a3b22def4c1796d0711
Sathish Velayudam <sathish.velayudam@gmail.com>
2020-06-07 09:03:50 -0400

    Updates

---
+++ precache-manifest.215b06ebcae63ed8451d8739fdd76658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ace9c71af73498700bbe780d13fc9e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f389d3a3d4421bf1cd67",
+    "url": "/react-tutorial/static/css/main.1324ef48.chunk.css"
+  },
+  {
+    "revision": "4762f349b9c98d1da948",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.0bf4f24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f389d3a3d4421bf1cd67",
+    "url": "/react-tutorial/static/js/main.f7a7659c.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
